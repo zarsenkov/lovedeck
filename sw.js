1db427f2642754aabd52d1588e15f5b00b0d61a8
zarsenkov <74777354+zarsenkov@users.noreply.github.com>
2026-02-03 13:26:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ========== SERVICE WORKER С АВТООБНОВЛЕНИЕМ ==========
-const CACHE_NAME = 'lovecouple-v1.8.9'; // ⚡ МЕНЯЙТЕ ВЕРСИЮ ПРИ КАЖДОМ ОБНОВЛЕНИИ!
+const CACHE_NAME = 'lovecouple-v1.9.0'; // ⚡ МЕНЯЙТЕ ВЕРСИЮ ПРИ КАЖДОМ ОБНОВЛЕНИИ!
 const urlsToCache = [
   '/',
   '/index.html',
